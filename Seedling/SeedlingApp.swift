d03bf0bf37645977670c9c0abf3274373362410c
Joshua Grant <joshgrant@pm.me>
2022-09-13 21:14:43 -0600

    Initial Commit

---
+++ Seedling/SeedlingApp.swift
@@ -0,0 +1,20 @@
+//
+//  SeedlingApp.swift
+//  Seedling
+//
+//  Created by Joshua Grant on 9/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SeedlingApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
