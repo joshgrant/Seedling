7a6445603aea3972968aee626082d9e5181787c4
Jahesse <113477788+Jahesse2004@users.noreply.github.com>
2022-10-24 16:40:16 -0600

    74 create the task cell (#75)
    * Button
    
    * Button change
    
    * Created task cell, Fixed button, Added checked state
    
    * Editable text can be stricken, Added underline
    
    * added spacing, multiline text
    
    * Changed spacing between tasks, demo data to previews, added padding to the sides of long tasks
    
    * Added accessibility label
    
    * Updated the workflow
    
    * Updated the workflow
    
    * Updated the workflow
    
    * Updated the workflow
    
    * Updated the workflow
    
    * Reverted workflow to older version
    
    * Added SeedlingTests
    
    * Updating the gitignore
    
    Co-Authored-By: Josh Grant <joshgrant@pm.me>
    
    Co-authored-by: Josh Grant <joshgrant@pm.me>

---
+++ Seedling/Localizable.swift
@@ -0,0 +1,14 @@
+//
+//  Localizable.swift
+//  Seedling
+//
+//  Created by Jesse Vernon on 10/17/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Localizable
+{
+    static let taskPlaceholder: LocalizedStringKey = "task_placeholder"
+}
