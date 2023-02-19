43a561e339a896ee6c05b42c0524708374598719
ccwemett <ccwemett@berkeley.edu>
2023-02-19 11:08:24 -0800

    Integrating V2

--- Seedling/Project.swift
+++ Seedling/Project.swift
@@ -20,5 +20,13 @@ let project = Project(
               deploymentTarget: .iOS(targetVersion: "15.0", devices: [.ipad, .iphone]),
               infoPlist: .none,
               sources: ["Tests/**"],
+              settings: .settings(base: ["GENERATE_INFOPLIST_FILE": "YES"])),
+        .init(name: "SeedlingUITests",
+              platform: .iOS,
+              product: .uiTests,
+              bundleId: "joshgrant.me.seedlingUITests",
+              deploymentTarget: .iOS(targetVersion: "15.0", devices: [.ipad, .iphone]),
+              infoPlist: .none,
+              sources: ["UITests/**"],
               settings: .settings(base: ["GENERATE_INFOPLIST_FILE": "YES"]))
     ])
